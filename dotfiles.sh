97d44b6513f3ad6861919e6d3abda94f9eb26a56
janmarius <janmarius_91@hotmail.com>
2020-02-01 22:09:35 +0100

    Fixed bug in recursive function (dotfiles.sh)

--- dotfiles.sh
+++ dotfiles.sh
@@ -5,59 +5,49 @@ DIRECTORY2=~/
 DIRECTORY3=~/dotfiles_old/
 
 CREATE_SYMBOLIC_LINKS () {
-	CURRENT_DIRECTORY=$1
-	HOME_DIRECTORY=$2	
-	OLD_DIRECTORY=$3
 	DIRECTORIES="."
 	FILES=""
 
 	# Find all files and directories
-	ELEMENTS=`ls -A $CURRENT_DIRECTORY`
+	ELEMENTS=`ls -A $1`
 	for ELEMENT in $ELEMENTS;
 	do
-		if [[ -L $CURRENT_DIRECTORY/$ELEMENT ]]; then
+		if [[ -L $1$ELEMENT ]]; then
 			echo "Error: Found symbolic link in your dotfiles directory!"
-		elif [ \( -d $CURRENT_DIRECTORY/$ELEMENT \) -a  ! \( "$ELEMENT" ==  ".git" \) ]; then
+		elif [ \( -d $1$ELEMENT \) -a  ! \( "$ELEMENT" ==  ".git" \) ]; then
 			DIRECTORIES="$DIRECTORIES $ELEMENT/"
-		elif [ \( -f $CURRENT_DIRECTORY/$ELEMENT \) -a ! \( "$ELEMENT" == "dotfiles.sh" \) -a ! \( "$ELEMENT" == "README.md" \) ]; then
+		elif [ \( -f $1$ELEMENT \) -a ! \( "$ELEMENT" == "dotfiles.sh" \) -a ! \( "$ELEMENT" == "README.md" \) ]; then
 			FILES="$FILES $ELEMENT"
 		fi
 	done
 
-
 	for DIRECTORY in $DIRECTORIES;
-	do
-		echo "Creating $OLD_DIRECTORY$DIRECTORY for backup of any old dotfiles"
-		mkdir -p $OLD_DIRECTORY$DIRECTORY
-		echo "...done"
+	do	
+		# Creating dotfiles_old directory
+		echo "Creating $3$DIRECTORY for backup of any old dotfiles"	
+		mkdir -p $3$DIRECTORY
+
+		# Creating directory for symlinks
+		echo "Creating $2$DIRECTORY for symlinks"
+		mkdir -p $2$DIRECTORY
 		
-		# Cleaning up $HOME_DIRECTORY$DIRECTORY
-		echo "Cleaning up $HOME_DIRECTORY$DIRECTORY"
+		# Cleaning up directory
+		echo "Cleaning up $2"
 		for FILE in $FILES; do
-			if [ \( -L $HOME_DIRECTORY$FILE \) ]; then
+			if [ \( -L $2$FILE \) ]; then
 				echo "Deleting existing symbolic link file: $FILE"
-				rm $HOME_DIRECTORY$FILE
+				rm $2$FILE
 			fi
-			if [ \( -e $HOME_DIRECTORY$FILE \) -a ! \( -L $HOME_DIRECTORY$FILE \) ]; then
-				echo "Moving $FILE to ~/dotfiles_old/.config/i3/$FILE"
-				mv $HOME_DIRECTORY$FILE $OLD_DIRECTORY$FILE
+			if [ \( -e $2$FILE \) -a ! \( -L $2$FILE \) ]; then
+				echo "Moving $2$FILE to $3$FILE"
+				mv $2$FILE $3$FILE
 			fi
 		done
 		echo "...done"
-		
-
+		echo ""
 
 		if [ $DIRECTORY != "." ]; then
-			SAVE_CURRENT_DIRECTORY=$CURRENT_DIRECTORY
-			SAVE_HOME_DIRECTORY=$HOME_DIRECTORY
-			SAVE_OLD_DIRECTORY=$OLD_DIRECTORY
-			SAVE_FILES=$FILES
-				
-			CREATE_SYMBOLIC_LINKS $CURRENT_DIRECTORY$DIRECTORY $HOME_DIRECTORY$DIRECTORY $OLD_DIRECTORY$DIRECTORY
-
-			CURRENT_DIRECTORY=$SAVE_CURRENT_DIRECTORY
-			HOME_DIRECTORY=$SAVE_HOME_DIRECTORY
-			OLD_DIRECTORY=$SAVE_OLD_DIRECTORY
+			CREATE_SYMBOLIC_LINKS $1$DIRECTORY $2$DIRECTORY $3$DIRECTORY
 		fi
 	done
 
