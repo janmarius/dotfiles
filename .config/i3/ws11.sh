98a030e6ea1d2db03e8b333ed255872a63c30ee6
janmarius <janmarius_91@hotmail.com>
2020-02-06 03:10:29 +0100

    Added android emulator to ws11.sh

--- .config/i3/ws11.sh
+++ .config/i3/ws11.sh
@@ -2,10 +2,14 @@
 
 WORKSPACE=$1
 CODE_EDITOR=$2
+ANDROID_EMULATOR=$3
 
 i3-msg "workspace $WORKSPACE; append_layout ~/.config/i3/workspace-11.json"
 
 # Code Editor
 $CODE_EDITOR & disown
+
+# Android Emulator
+$(cd ~/Android/Sdk/emulator ; ./emulator -avd $3) & disown
 
 exit 0
