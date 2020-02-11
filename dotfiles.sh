c5d447775e97db7e55e8c6097eef20d64d7c168e
janmarius <janmarius_91@hotmail.com>
2020-02-11 19:37:58 +0100

    Added colors

--- dotfiles.sh
+++ dotfiles.sh
@@ -16,7 +16,7 @@ CREATE_SYMBOLIC_LINKS () {
 	for ELEMENT in ${ELEMENTS[@]};
 	do
 		if [[ -L $1$ELEMENT ]]; then
-			echo "Error: Found symbolic link in your dotfiles directory!"
+			echo -e "\e[41mError: Found symbolic link in your dotfiles directory!\e[0m"
 		elif [ \( -d $1$ELEMENT \) -a  ! \( "$ELEMENT" ==  ".git" \) ]; then
 			DIRECTORIES+=($ELEMENT/)
 		elif [ \( -f $1$ELEMENT \) -a ! \( "$ELEMENT" == "dotfiles.sh" \) -a ! \( "$ELEMENT" == "README.md" \) ]; then
@@ -26,6 +26,7 @@ CREATE_SYMBOLIC_LINKS () {
 
 	for DIRECTORY in ${DIRECTORIES[@]};
 	do	
+		echo -e "\e[33mCreating necessary directories\e[0m"
 		# Creating dotfiles_old directory
 		echo "Creating $3$DIRECTORY for backup of any old dotfiles"	
 		mkdir -p $3$DIRECTORY
@@ -33,9 +34,11 @@ CREATE_SYMBOLIC_LINKS () {
 		# Creating directory for symlinks
 		echo "Creating $2$DIRECTORY for symlinks"
 		mkdir -p $2$DIRECTORY
-		
+		echo -e "\e[32m...done\e[0m"
+		echo ""
+
 		# Cleaning up directory
-		echo "Cleaning up $2"
+		echo -e "\e[33mCleaning up $2\e[0m"
 		for FILE in ${FILES[@]}; do
 			if [ \( -L $2$FILE \) ]; then
 				echo "Deleting existing symbolic link file: $FILE"
@@ -46,7 +49,7 @@ CREATE_SYMBOLIC_LINKS () {
 				mv $2$FILE $3$FILE
 			fi
 		done
-		echo "...done"
+		echo -e "\e[32m...done\e[0m"
 		echo ""
 
 		if [ $DIRECTORY != "." ]; then
@@ -61,19 +64,19 @@ CREATE_SYMBOLIC_LINKS () {
 	for FILE in ${ALL_FILES[@]};
 	do
 		if [[ -L $1$FILE ]]; then
-			echo "Error: Found symbolic link in your dotfiles directory!"
+			echo -e "\e[41mError: Found symbolic link in your dotfiles directory!\e[0m"
 		elif [ \( -f $1$FILE \) -a ! \( "$FILE" == "dotfiles.sh" \) -a ! \( "$FILE" == "README.md" \) ]; then
 			SYMLINK_FILES+=($FILE)
 		fi
 	done
 
 	# Creating symbolic link files for all dotfiles
-	echo "Creating symbolic link files for all dotfiles"
+	echo -e "\e[33mCreating symbolic link files for all dotfiles\e[0m"
 	for FILE in ${SYMLINK_FILES[@]}; do
 		echo "Creating symbolic link: ln -s $1$FILE $2$FILE"
 		ln -s $1$FILE $2$FILE
 	done
-	echo "...done"
+	echo -e "\e[32m...done\e[0m"
 	echo ""
 }
 
