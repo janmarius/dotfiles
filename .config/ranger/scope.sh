c85782c03b277017d12066b0ceed30a07382e469
janmarius <janmarius_91@hotmail.com>
2020-05-05 23:55:04 +0200

    Fixed image preview for RAW photos in ranger

--- .config/ranger/scope.sh
+++ .config/ranger/scope.sh
@@ -138,6 +138,20 @@ handle_image() {
         #           - "${IMAGE_CACHE_PATH}" < "${FILE_PATH}" \
         #           && exit 6 || exit 1;;
 
+		# BEGIN SNIPPET
+        image/x-canon-cr2|image/x-olympus-orf|image/tiff)
+            local orientation
+            # extract orientation from RAW file using exiftool (identify won't work)
+            orientation=$( exiftool -b -Orientation "${FILE_PATH}")
+            exiftool -b -PreviewImage "${FILE_PATH}" > "${IMAGE_CACHE_PATH}"
+            if [[ -n "$orientation" && "$orientation" != 1 ]]; then
+                # ...auto-rotate the image according to the EXIF data.
+                exiftool -overwrite_original_in_place -Orientation="$orientation" -n "${IMAGE_CACHE_PATH}"
+                mogrify -auto-orient "${IMAGE_CACHE_PATH}"
+            fi
+            exit 6;;
+        # END SNIPPET
+
         ## Image
         image/*)
             local orientation
