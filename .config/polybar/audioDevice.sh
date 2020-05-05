4d9364930f9fc80bb79f8bed65e67f4764bc085d
janmarius <janmarius_91@hotmail.com>
2020-05-05 23:21:27 +0200

    Added icon for closed streams

--- .config/polybar/audioDevice.sh
+++ .config/polybar/audioDevice.sh
@@ -1,8 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 
 DEVICE=`pactl list short sink-inputs | grep -n 1 | awk -F' ' '{print $2}'`
 if [ "$DEVICE" == "0" ]; then
 	echo ""
-elif [ "$DEVICE" == "7" ]; then
+elif [ "$DEVICE" == "1" ]; then
 	echo ""
+elif [ "$DEVICE" == "" ]; then
+	echo ""
 fi
